b04d6648e5db7ba5cd9056d14877a3a2038843be
zhon <zhon@xputah.org>
2014-05-13 09:43:28 -0600

    Added classes to catalog.js

---
+++ app/catalog/js/catalog.js
@@ -0,0 +1,32 @@
+[
+  // core
+  { "class": "Astronomy" },
+  { "class": "Charms" },
+  { "class": "Defence Against the Dark Arts" },
+  { "class": "Flying" },
+  { "class": "Herbology" },
+  { "class": "History of Magic" },
+  { "class": "Potions" },
+  { "class": "Transfiguration" },
+  // electives after 3rd year
+  { "class": "Study of Ancient Runes"},
+  { "class": "Arithmancy"},
+  { "class": "Muggle Studies"},
+  { "class": "Care of Magical Creatures"},
+  { "class": "Divination"},
+  // 6th year
+  { "class": "Apparition"},
+  { "class": "Alchemy"},
+  // Extra-curricular subjects
+  { "class": "Ancient Studies"},
+  { "class": "Art"},
+  { "class": "Earth Magic"},
+  { "class": "Muggle Art"},
+  { "class": "Music"},
+  { "class": "Muggle Music"},
+  { "class": "Ghoul Studies"},
+  { "class": "Magical Theory"},
+  { "class": "Xylomancy"},
+  { "class": "Frog Choir"},
+  { "class": "Hogwarts orchestra"},
+]
