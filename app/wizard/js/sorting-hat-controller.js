c4f522ada71d6d0bd8410f6f9cf877ce5e498056
amy-dredge <amy-dredge@pluralsight.com>
2014-05-26 10:14:49 -0600

    Code to make call to house repository

--- app/wizard/js/sorting-hat-controller.js
+++ app/wizard/js/sorting-hat-controller.js
@@ -3,4 +3,5 @@
 hogwartsApp
     .controller("SortingHatController", ['$scope', 'WizardRepository', 'HouseRepository', function ($scope, wizardRepository, houseRepository) {
 		$scope.wizard = wizardRepository.get();
+		houseRepository.get();
 	}]);
