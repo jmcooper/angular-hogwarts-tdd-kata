c18845bb595e15ed2fbe557dbf93658ad74e5469
amy-dredge <amy-dredge@pluralsight.com>
2014-05-26 11:51:08 -0600

    Failing test to make call to house assignment service

--- Tests/wizard/sorting-hat-controller-specs.js
+++ Tests/wizard/sorting-hat-controller-specs.js
@@ -1,15 +1,22 @@
 "use strict";
 
 describe('SortingHatController', function () {
-    var scope;
+    var scope, mockHouseAssignmentService;
 
     beforeEach(function () {
         module("hogwartsApp");
 
-        inject(function ($rootScope, $controller) {
+        inject(function ($rootScope, $controller, HouseAssignmentService) {
             scope = $rootScope.$new();         
+			mockHouseAssignmentService = sinon.stub(HouseAssignmentService);
 
-            $controller("SortingHatController", { $scope: scope });
+            $controller("SortingHatController", { $scope: scope, HouseAssignmentService: mockHouseAssignmentService });
         });
     });
+	
+	describe('when using the sorting hat', function(){
+		it('should sort the wizard', function(){
+			expect(mockHouseAssignmentService.assignWizard.called).toBeTruthy();
+		});
+	});
 });
