b1b08fbe1fe5ed1e9f40f5adfa2e74c8c5764916
amy-dredge <amy-dredge@pluralsight.com>
2014-05-26 11:53:22 -0600

    Code to make test pass - assign wizard when using sorting hat

--- app/wizard/js/sorting-hat-controller.js
+++ app/wizard/js/sorting-hat-controller.js
@@ -1,6 +1,8 @@
 'use strict';
 
 hogwartsApp
-    .controller("SortingHatController", ['$scope', function ($scope) {
-		
+    .controller("SortingHatController", ['$scope', 'HouseAssignmentService', function ($scope, houseAssignmentService) {
+		$scope.sort = function(){
+			houseAssignmentService.assignWizard();
+		};		
 	}]);
