73e7a1ea0f4eb5d1be243c9d27e4d6f6629feab7
amy-dredge <amy-dredge@pluralsight.com>
2014-05-26 11:47:42 -0600

    Undoing bad changes to sorting hat controller - ignore sorting hat controller changes up to this point

--- Tests/wizard/sorting-hat-controller-specs.js
+++ Tests/wizard/sorting-hat-controller-specs.js
@@ -1,38 +1,15 @@
 "use strict";
 
 describe('SortingHatController', function () {
-    var scope, mockWizardRepository, mockHouseRepository;
-    var wizard = {classes: [{id: 'foo'}]};
-	var houseOptions = ['one', 'two', 'three', 'four'];
+    var scope;
 
     beforeEach(function () {
         module("hogwartsApp");
 
-        inject(function ($rootScope, $controller, WizardRepository, HouseRepository) {
-            scope = $rootScope.$new();
+        inject(function ($rootScope, $controller) {
+            scope = $rootScope.$new();         
 
-            mockWizardRepository = sinon.stub(WizardRepository);
-            mockWizardRepository.get.returns(wizard);
-			
-			mockHouseRepository = sinon.stub(HouseRepository);
-			mockHouseRepository.get.returns(houseOptions);
-
-            $controller("SortingHatController", { $scope: scope, WizardRepository: mockWizardRepository, HouseRepository: mockHouseRepository });
-        });
-    });
-
-    describe('when the controller first loads', function () {
-        it('should get the wizard from the repository', function () {
-            expect(mockWizardRepository.get.called).toBeTruthy();
-        });
-        it('should put wizard on the scope', function() {
-            expect(scope.wizard).toEqual(wizard)
-        });
-		it('should get the house options from the repository', function() {
-            expect(mockHouseRepository.get.called).toBeTruthy();
-        });
-		it('should put houses on the scope', function() {
-            expect(scope.houseOptions).toEqual(houseOptions);
+            $controller("SortingHatController", { $scope: scope });
         });
     });
 });
--- app/wizard/js/sorting-hat-controller.js
+++ app/wizard/js/sorting-hat-controller.js
@@ -1,7 +1,6 @@
 'use strict';
 
 hogwartsApp
-    .controller("SortingHatController", ['$scope', 'WizardRepository', 'HouseRepository', function ($scope, wizardRepository, houseRepository) {
-		$scope.wizard = wizardRepository.get();
-		$scope.houseOptions = houseRepository.get();
+    .controller("SortingHatController", ['$scope', function ($scope) {
+
 	}]);
