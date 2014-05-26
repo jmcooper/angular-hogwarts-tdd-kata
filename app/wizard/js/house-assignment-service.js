540caac1b2b90772963f3e7232496c81b5a776ef
amy-dredge <amy-dredge@pluralsight.com>
2014-05-26 11:40:20 -0600

    Code to make test pass - get random number from service

--- app/wizard/js/house-assignment-service.js
+++ app/wizard/js/house-assignment-service.js
@@ -1,10 +1,11 @@
 'use strict';
 
-hogwartsApp.factory('HouseAssignmentService', ['WizardRepository', 'HouseRepository', function(wizardRepository, houseRepository) {
+hogwartsApp.factory('HouseAssignmentService', ['WizardRepository', 'HouseRepository', 'RandomNumberService', function(wizardRepository, houseRepository, randomNumberService) {
     return {
         assignWizard: function(){
 			var wizard = wizardRepository.get();
 			var houseOptions = houseRepository.get();
+			var randomHouseIndex = randomNumberService.getInRange(0, houseOptions.length - 1);
 		}
     };
 }]);
