07cf96827dee40fb1aaac39dbd60da20b8604d0d
amy-dredge <amy-dredge@pluralsight.com>
2014-05-26 11:41:38 -0600

    Code to make test pass - save wizard with the house

--- app/wizard/js/house-assignment-service.js
+++ app/wizard/js/house-assignment-service.js
@@ -6,6 +6,8 @@ hogwartsApp.factory('HouseAssignmentService', ['WizardRepository', 'HouseReposit
 			var wizard = wizardRepository.get();
 			var houseOptions = houseRepository.get();
 			var randomHouseIndex = randomNumberService.getInRange(0, houseOptions.length - 1);
+			wizard.house = houseOptions[randomHouseIndex];
+			wizardRepository.save(wizard);
 		}
     };
 }]);
