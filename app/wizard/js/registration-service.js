33165d54a680f70bc3f4d006b51f8122234fdb2b
Jim Cooper <jim-cooper@pluralsight.com>
2014-05-25 18:24:46 -0600

    Passing test (Green, fake-it)

--- app/wizard/js/registration-service.js
+++ app/wizard/js/registration-service.js
@@ -3,7 +3,7 @@
 hogwartsApp.factory('RegistrationService', function() {
     return {
         register: function() {
-
+            return {success: false, message: "Course does not exist"};
         }
     };
 });
