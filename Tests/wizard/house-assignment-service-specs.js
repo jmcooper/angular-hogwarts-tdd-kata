7c7cfd4e2a2005016caa68cd18ef7f5098f98156
amy-dredge <amy-dredge@pluralsight.com>
2014-05-26 11:37:15 -0600

    Failing test to get house options from repository in house assignment service

--- Tests/wizard/house-assignment-service-specs.js
+++ Tests/wizard/house-assignment-service-specs.js
@@ -1,26 +1,33 @@
 "use strict";
 
 describe('HouseAssignmentService', function () {
-    var houseAssignmentService, mockWizardRepository;	
+    var houseAssignmentService, mockWizardRepository, mockHouseRepository;
 	
     beforeEach(function () {
         module("hogwartsApp");
 
-        inject(function (HouseAssignmentService, WizardRepository) {
+        inject(function (HouseAssignmentService, WizardRepository, HouseRepository) {
             houseAssignmentService = HouseAssignmentService;            
-            mockWizardRepository = sinon.stub(WizardRepository);           
+            mockWizardRepository = sinon.stub(WizardRepository); 
+			mockHouseRepository = sinon.stub(HouseRepository);			
         });
     });
 
     describe('when assigning a wizard to a house', function () { 
 		var wizard = {house:""};
+		var houseOptions = ['one', 'two', 'three', 'four'];	
 		
         beforeEach(function() {            
             mockWizardRepository.get.returns(wizard);
+			mockHouseRepository.get.returns(houseOptions);
+			
 			houseAssignmentService.assignWizard();
         });
         it('should get the wizard from the repository', function () {
             expect(mockWizardRepository.get.called).toBeTruthy();
-        });       
+        });     
+		it('should get the house options from the repository', function() {
+            expect(mockHouseRepository.get.called).toBeTruthy();
+        }); 		
     });    
 });
