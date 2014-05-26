5fdf1b8182f4f75dcda4dee7517fe3a171fa7e78
Jim Cooper <jim-cooper@pluralsight.com>
2014-05-25 19:10:59 -0600

    Preventing wizard from double registering (Green)

--- app/wizard/js/registration-service.js
+++ app/wizard/js/registration-service.js
@@ -7,6 +7,9 @@ hogwartsApp.factory('RegistrationService', ['CatalogRepository', 'WizardReposito
             if (!course)
                 return {success: false, message: "Course does not exist"};
 
+            if (isWizardAlreadyRegisteredForCourse(courseId))
+                return {success: false, message: 'You are already registered for that course'};
+
             registerWizardForCourse(course);
 
             return {success: true, message: ''};
@@ -24,6 +27,14 @@ hogwartsApp.factory('RegistrationService', ['CatalogRepository', 'WizardReposito
         var wizard = wizardRepository.get();
         wizard.classes.push(course);
         wizardRepository.save(wizard);
+    }
+
+    function isWizardAlreadyRegisteredForCourse(courseId) {
+        var wizard = wizardRepository.get();
+        for (var i = 0; i < wizard.classes.length; i++) {
+            if (wizard.classes[i].id === courseId) return true;
+        }
+        return false;
     }
 }]);
 
