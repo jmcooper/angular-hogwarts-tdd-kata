e52d23550ae4a392aed36f8e7141a1e6a8f40489
amy-dredge <amy-dredge@pluralsight.com>
2014-05-26 11:38:00 -0600

    Code to make test pass - get house options from repository in house assignment service

--- app/wizard/js/house-assignment-service.js
+++ app/wizard/js/house-assignment-service.js
@@ -1,9 +1,10 @@
 'use strict';
 
-hogwartsApp.factory('HouseAssignmentService', ['WizardRepository', function(wizardRepository) {
+hogwartsApp.factory('HouseAssignmentService', ['WizardRepository', 'HouseRepository', function(wizardRepository, houseRepository) {
     return {
         assignWizard: function(){
 			var wizard = wizardRepository.get();
+			var houseOptions = houseRepository.get();
 		}
     };
 }]);
