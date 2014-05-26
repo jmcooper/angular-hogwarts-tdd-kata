3bc63eeb99465917a16a0baeaf99170c247b09a0
amy-dredge <amy-dredge@pluralsight.com>
2014-05-26 11:39:32 -0600

    Failing test to get random number from service in house assignment service

--- Tests/wizard/house-assignment-service-specs.js
+++ Tests/wizard/house-assignment-service-specs.js
@@ -1,25 +1,28 @@
 "use strict";
 
 describe('HouseAssignmentService', function () {
-    var houseAssignmentService, mockWizardRepository, mockHouseRepository;
+    var houseAssignmentService, mockWizardRepository, mockHouseRepository, mockRandomNumberService;
 	
     beforeEach(function () {
         module("hogwartsApp");
 
-        inject(function (HouseAssignmentService, WizardRepository, HouseRepository) {
+        inject(function (HouseAssignmentService, WizardRepository, HouseRepository, RandomNumberService) {
             houseAssignmentService = HouseAssignmentService;            
             mockWizardRepository = sinon.stub(WizardRepository); 
-			mockHouseRepository = sinon.stub(HouseRepository);			
+			mockHouseRepository = sinon.stub(HouseRepository);	
+			mockRandomNumberService = sinon.stub(RandomNumberService);			
         });
     });
 
     describe('when assigning a wizard to a house', function () { 
 		var wizard = {house:""};
 		var houseOptions = ['one', 'two', 'three', 'four'];	
+		var houseNumber = 1;
 		
         beforeEach(function() {            
             mockWizardRepository.get.returns(wizard);
 			mockHouseRepository.get.returns(houseOptions);
+			mockRandomNumberService.getInRange.returns(houseNumber);
 			
 			houseAssignmentService.assignWizard();
         });
@@ -28,6 +31,9 @@ describe('HouseAssignmentService', function () {
         });     
 		it('should get the house options from the repository', function() {
             expect(mockHouseRepository.get.called).toBeTruthy();
-        }); 		
+        }); 	
+		it('should get a random number for house options', function() {
+            expect(mockRandomNumberService.getInRange.calledWith(0, houseOptions.length - 1)).toBeTruthy();
+        });			
     });    
 });
