cb0138bd0df25fe0fea62597280d5e048a7e69fa
Jim Cooper <jim-cooper@pluralsight.com>
2014-05-25 18:35:39 -0600

    Implemented happy path (Green)

--- app/wizard/js/registration-service.js
+++ app/wizard/js/registration-service.js
@@ -1,10 +1,20 @@
 'use strict';
 
-hogwartsApp.factory('RegistrationService', function() {
+hogwartsApp.factory('RegistrationService', ['CatalogRepository', function(catalogRepository) {
     return {
-        register: function() {
-            return {success: false, message: "Course does not exist"};
+        register: function(courseId) {
+            if (!doesCourseExist(courseId))
+                return {success: false, message: "Course does not exist"};
+            return {success: true, message: ''};
         }
     };
-});
+
+    function doesCourseExist(courseId) {
+        var catalog = catalogRepository.getCatalog();
+        for (var i = 0; i<catalog.length; i++) {
+            if (catalog[i].id === courseId) return true;
+        }
+        return false;
+    }
+}]);
 
