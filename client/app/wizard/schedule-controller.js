43dd0abdd837441d8a25e9cefa0739f67d1ea5ac
zhon <zhon@xputah.org>
2014-05-31 10:22:17 -0600

    Removed start code

--- client/app/wizard/schedule-controller.js
+++ client/app/wizard/schedule-controller.js
@@ -2,5 +2,5 @@
 
 hogwartsApp
     .controller("ScheduleController", ['$scope', 'WizardRepository', function ($scope, wizardRepository) {
-        $scope.wizard = wizardRepository.get();
+
     }]);
