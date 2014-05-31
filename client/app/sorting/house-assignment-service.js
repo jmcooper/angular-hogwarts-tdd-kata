b70df794a49333ce1027a95b040b7abf86c6a53f
zhon <zhon@xputah.org>
2014-05-31 17:25:10 -0600

    convert tabs to spaces

--- client/app/sorting/house-assignment-service.js
+++ client/app/sorting/house-assignment-service.js
@@ -2,15 +2,15 @@
 
 hogwartsApp.factory('HouseAssignmentService', ['WizardRepository', 'HouseRepository', 'RandomNumberService', function(wizardRepository, houseRepository, randomNumberService) {
     return {
-        assignWizard: function(){
-			var wizard = wizardRepository.get();
-			var houseOptions = houseRepository.get();
-			var randomHouseIndex = randomNumberService.getInRange(0, houseOptions.length - 1);
-			var selectedHouse = houseOptions[randomHouseIndex];
-			wizard.house = selectedHouse;
-			wizardRepository.save(wizard);
-			return selectedHouse;
-		}
+        assignWizard: function() {
+            var wizard = wizardRepository.get();
+            var houseOptions = houseRepository.get();
+            var randomHouseIndex = randomNumberService.getInRange(0, houseOptions.length - 1);
+            var selectedHouse = houseOptions[randomHouseIndex];
+            wizard.house = selectedHouse;
+            wizardRepository.save(wizard);
+            return selectedHouse;
+        }
     };
 }]);
 
