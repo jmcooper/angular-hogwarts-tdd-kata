ae5925f207bc01469410ef2d2a751ddd4cbbf683
zhon <zhon@xputah.org>
2014-06-01 01:56:51 -0600

    RandomNumberService return 3 (Hufflepuff)

--- app/sorting/random-number-service.js
+++ app/sorting/random-number-service.js
@@ -3,8 +3,9 @@
 hogwartsApp.factory('RandomNumberService', [function() {
     return {
         getInRange: function(min, max){
-			return Math.floor(Math.random() * (max - min + 1)) + min;
-		}
+            return 3;
+            //return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
     };
 }]);
 
