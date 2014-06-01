7a7de12f7dcf4884f80af2c4a885c33f2a9b81e6
zhon <zhon@xputah.org>
2014-06-01 02:08:55 -0600

    Random is comples but not random

--- app/sorting/random-number-service.js
+++ app/sorting/random-number-service.js
@@ -3,8 +3,7 @@
 hogwartsApp.factory('RandomNumberService', [function() {
     return {
         getInRange: function(min, max){
-            return 3;
-            //return Math.floor(Math.random() * (max - min + 1)) + min;
+            return Math.floor(Math.random() * (max - max)) + max;
         }
     };
 }]);
