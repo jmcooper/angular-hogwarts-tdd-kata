534441506c69274743221bfb90941fad31d065ba
zhon <zhon@xputah.org>
2014-06-12 11:14:36 -0600

    Lots of code changes

--- app/catalog/catalog-controller.js
+++ app/catalog/catalog-controller.js
@@ -1,6 +1,15 @@
 'use strict';
 
 hogwartsApp
-.controller("CatalogController", ['$scope', function ($scope) {
+.controller("CatalogController", [ '$scope',
+                                    'CatalogRepository',
+                                    'RegistrationService',
+                                    function ($scope, catalogRepository, registrationService) {
+    $scope.catalog = catalogRepository.getCatalog();
+
+
+    $scope.register = function(courseId) {
+        $scope.response = registrationService.register(courseId);
+    };
 
 }]);
