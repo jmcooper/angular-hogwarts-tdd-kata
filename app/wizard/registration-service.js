965cd697e7ea548a1edc0b0bcd62bbc7269d81a6
zhon <zhon@xputah.org>
2014-06-13 07:10:13 -0600

    Starting state

--- app/wizard/registration-service.js
+++ app/wizard/registration-service.js
@@ -4,21 +4,6 @@ hogwartsApp
 .factory('RegistrationService', [ 'CatalogRepository',
                                   'WizardRepository',
                                   function(catalogRepository, wizardRepository) {
-    return {
-        register: function(courseId) {
-            var course = catalogRepository.getCourse(courseId),
-                wizard = wizardRepository.get();
-
-            return registerWizardForCourse(wizard, course);
-        }
-    };
-
-
-    function registerWizardForCourse(wizard, course) {
-        wizard.courses[course.id] = course;
-        wizardRepository.save(wizard);
-        return {success: true};
-    }
 
 }]);
 
